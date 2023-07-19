a2123c63aaaa8fd6a639264c1ea6fc199c8c17e9
Damien Tanner <damien@tanner.me>
2023-07-19 12:11:01 +0100

    Update projects.js

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -27,22 +27,12 @@ let PROJECTS = [
   },
 ]
 
-// Route layouts get passed the same props as the route's view
-// TODO find a way to pull out the view's props and pass them here too
-const ProjectsLayout = (props) => html`
-<div class="p-6">
-  <h1 class="text-2xl">Projects Layout</h1>
-  ${props.children}
-</div>
-`
-
 const projectsRoute = new Hono()
 
 // TODO run the actual route code when rendering each layout, and then put all the returned values into an object we pass along, like Remix does with routes. This way we don't have to make sure each route loads all the data that all the layouts it uses needs
 projectsRoute.use('/*', layout(MainLayout))
 projectsRoute.get('/', view(({ context }) => {
   const projects = PROJECTS
-  // return c.text`get / Projects Route`
   return ListProjects({ projects })
 }))
 // TODO write examples of the two routes below, but returning JSX inline, and also extractin into its own component and calling like layout(ViewProject)
@@ -82,7 +72,7 @@ projectsRoute.put('/:projectId/todos/:todoId', view(async ({ context }) => {
 
   const project = PROJECTS[projectId]
   const todo = project.todos[todoId]
-  // TODO we must revalidate ViewProject to refesh the list of todos
+
   return ViewTodo({
     project,
     todo,
